d0160a4bca60ab1e6cb7693c60e620e388a88307
theo <theo.hermann@epfl.ch>
2019-05-15 14:26:10 +0200

    fusion yucef + theo

--- 288453_282844/tools.cc
+++ 288453_282844/tools.cc
@@ -101,7 +101,7 @@ double calcul_angle(Coord pred, Coord cible)
 {
 	double tan_a(0);
 	tan_a=(cible.y-pred.y)/(cible.x-pred.x);
-	if(std::abs(pred.y-cible.y)<PRECISION) 
+	if(std::abs(pred.y-cible.y)<PRECISION)
 	{
 		if(pred.x>=cible.x) return M_PI;
 		else return 0;
@@ -116,5 +116,62 @@ double calcul_angle(Coord pred, Coord cible)
 	  if(cible.y>=pred.y) return M_PI+atan(tan_a);
 	  else return atan(tan_a);
     }
-	
+
+}
+
+double pente_chemin(Coord j1, Coord j2)
+{
+	return ((fabs(j1.x-j2.x)) / (fabs(j1.y-j2.y)));
+}
+
+double calcul_b(Coord predateur, double pente)
+{
+	return predateur.y - pente*predateur.y;
+}
+
+bool carre_dans_zone(Rond p1, Rond p2, Coord carre, int nbCell)					//faire un passage par référence pour faire une bonne copie
+{
+	if((p1.x<=p2.x) && (p1.y<=p2.y))
+	{
+		if((carre.x<p1.x-ML-MOIT_HAUT-p1.rayon) ||
+								(carre.x>p2.x+ML+MOIT_HAUT+p2.rayon))			//cas ou le point le plus haut est à gauche
+			return false;
+		if((carre.y<p1.y-ML-MOIT_HAUT-p1.rayon) ||
+								(carre.y<p2.y+ML+MOIT_HAUT+p2.rayon))
+			return false;
+	}
+	if((p1.x>p2.x) && (p1.y<p2.y))
+	{
+		if((carre.x>p1.x+ML+MOIT_HAUT+p1.rayon) ||
+								(carre.x<p2.x-ML-MOIT_HAUT-p2.rayon))			//cas ou le point le plus haut est à droite
+			return false;
+		if((carre.y<p1.y-ML-MOIT_HAUT-p1.rayon) ||
+								(carre.y<p2.y+ML+MOIT_HAUT+p2.rayon))
+			return false;	
+	}
+	return true;
+}
+
+double fonct_maths(double x, double pente, double b)
+{
+	return x*pente + b;
+}
+
+bool sup_rect_obst(Coord obstacle, Rond j1, Rond j2, int nbCell)
+{
+	if (fabs(obstacle.x-j1.x)<=ML+MOIT_HAUT+j1.rayon)
+	{
+		if(j1.y<j2.y)
+			if((obstacle.y>j1.y)&&(obstacle.y<j2.y))
+				return true;
+		if(j1.y>j2.y)
+			if((obstacle.y<j1.y)&&(obstacle.y>j2.y))
+				return true;
+	}
+}
+
+Carre coord_en_indice(Coord point, int nbCell)
+{
+	double a=(point.x+DIM_MAX-MOIT_HAUT)/(SIDE/nbCell)
+
 }
--- 288453_282844/tools.h
+++ 288453_282844/tools.h
@@ -42,4 +42,16 @@ double norme(Coord v);
 
 double calcul_angle(Coord pred, Coord cible);
 
+double pente_chemin(Coord j1, Coord j2);
+
+double calcul_b(Coord predateur, double pente);
+
+double fct_math(double angle, double x, double b);
+
+bool carre_dans_zone(Rond p1, Rond p2, Coord carre, int nbCell);
+
+double fonct_maths(double x, double pente, double b);
+
+bool sup_rect_obst(Coord obstacle, Rond j1, Rond j2, int nbCell);
+
 #endif
